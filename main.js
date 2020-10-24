a764f515096a79d4677dcf9f6423f4e87d0141b9
suriya-05 <70579586+suriya-05@users.noreply.github.com>
2020-10-24 11:29:31 +0530

    Electron UI

---
+++ main.js
@@ -0,0 +1,72 @@
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: { nodeIntegration: true },
+    });
+
+    mainWindow.loadFile('index.html');
+
+    ipcMain.on('openImageFolderWindow', event => {
+        dialog
+            .showOpenDialog(mainWindow, {
+                buttonLabel: 'Select images folder',
+                defaultPath: app.getPath('desktop'),
+                properties: ['openDirectory'],
+            })
+            .then(result => {
+                event.reply('imageFolderPath', result.filePaths);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+    ipcMain.on('openTextFolderWindow', event => {
+        dialog
+            .showOpenDialog(mainWindow, {
+                buttonLabel: 'Select texts folder',
+                defaultPath: app.getPath('desktop'),
+                properties: ['openDirectory'],
+            })
+            .then(result => {
+                event.reply('textFolderPath', result.filePaths);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+    ipcMain.on('openUserOutputTextFolderWindow', event => {
+        dialog
+            .showOpenDialog(mainWindow, {
+                buttonLabel: 'Select user output texts folder',
+                defaultPath: app.getPath('desktop'),
+                properties: ['openDirectory'],
+            })
+            .then(result => {
+                event.reply('textFolderPath', result.filePaths);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) createWindow();
+});
